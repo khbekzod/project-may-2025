3dba3ac808c4c3c58a34a6b1e9ee3b81fcfc48ab
khbekzod <khabibullaevbekzodk@gmail.com>
2025-08-21 01:48:01 +0000

    bro, you need to fix all of this

---
+++ script.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# step 1 - prepare vm (install terraform and ansible)
+# step 2 - run terraform code
+# step 3 - get IP and update inventory file
+# step 4 - run ansible
+
+function prepare_vm() {
+    sudo apt update
+    sudo apt install ansible -y 
+    if [ ! -f /usr/share/keyrings/hashicorp-archive-keyring.gpg ]
+    then 
+    wget -O - https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
+    fi 
+    echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(grep -oP '(?<=UBUNTU_CODENAME=).*' /etc/os-release || lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+    sudo apt update && sudo apt install terraform
+}
+
+function create_ec2() {
+    cd terraform
+    terraform init
+    terraform apply -auto-approve
+}
+
+function update_ip() {
+    terraform output -raw ec2 > ../ansible/hosts
+}
+
+function install_apps() {
+    cd ../ansible
+    anisble-playbook main.yml
+}
+
+prepare_vm
+create_ec2
+update_ip
+install_apps
